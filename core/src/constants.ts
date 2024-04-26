57ca180e4e35083a18a07b93204f966a3fba21d3
samuelhulla <oficialny.hulla@gmail.com>
2024-04-26 18:05:14 +0200

    :white_check_mark: feat: constants

---
+++ core/src/constants.ts
@@ -0,0 +1,46 @@
+import type { Context } from './call'
+import type { Methods } from './types'
+
+type MethodsArray = readonly Methods[]
+export type LowercaseMethods = {
+  [K in Methods]: Lowercase<K>
+}[Methods]
+
+export const METHODS = [
+  'CONNECT',
+  'DELETE',
+  'GET',
+  'HEAD',
+  'OPTIONS',
+  'PATCH',
+  'POST',
+  'PUT',
+  'TRACE',
+] satisfies MethodsArray
+
+export const METHODS_LOWERCASE = [
+  'connect',
+  'delete',
+  'get',
+  'head',
+  'options',
+  'patch',
+  'post',
+  'put',
+  'trace',
+] satisfies LowercaseMethods[]
+
+export const RESERVED_CONTEXT = [
+  'route',
+  'method',
+  'route',
+  'args',
+  'type',
+  'routerName',
+] as const satisfies (keyof Context<string, any, any>)[]
+
+export const RESERVED_KEYWORDS = [
+  ...(METHODS.map((m) => m.toLowerCase()) as Lowercase<Methods>[]),
+  'call',
+  'context',
+] as const
