39eb483b202e039c98a0761438a3a850b98b739a
samuelhulla <oficialny.hulla@gmail.com>
2024-04-26 18:01:35 +0200

    :white_check_mark: feat: internal call

---
+++ core/src/call.ts
@@ -0,0 +1,35 @@
+import type { Args, Fn, Methods } from './types'
+
+export type CallNames = Lowercase<Methods> | 'call'
+export type Context<N extends string, CN extends CallNames, A extends Args, RR extends string = string> = {
+  method: CN
+  type: CN extends 'call' ? 'procedure' : 'request'
+  route: N
+  routerName: RR
+  args: A
+}
+
+export type ResolverArgs<CTX, R> = [R, CTX]
+
+export type Resolver<CTX, R, R2 = R> = Fn<ResolverArgs<CTX, R>, R2>
+
+export type Call<N extends string, CN extends CallNames, CTX, A extends Args, R, R2 = R> = {
+  route: N
+  fn: Fn<A, R>
+  resolver?: Resolver<CTX, R, R2>
+  method: CN
+}
+
+export function call<const N extends string, const CN extends CallNames, CTX, A extends Args, const R, const R2 = R>(
+  route: N,
+  method: CN,
+  fn: Fn<A, R>,
+  resolver?: Resolver<CTX, R, R2>
+): Call<N, CN, CTX, A, R, R2> {
+  return {
+    route,
+    fn,
+    resolver,
+    method,
+  }
+}
