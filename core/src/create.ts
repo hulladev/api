924f70f749c00b10d822a5686c0fdac7629b4d84
samuelhulla <oficialny.hulla@gmail.com>
2024-04-29 15:47:25 +0200

    :wrench: update: strict adapter types
    always require router as a first argument

--- core/src/create.ts
+++ core/src/create.ts
@@ -2,14 +2,26 @@ import type { Router, RouterAdapter, RouterShape } from './router'
 import type { Fn, Obj } from './types'
 import { entries, omit } from './util'
 
-type Adapters = Record<string, (...args: any[]) => any>
+type Adapters<Routes extends RouterShape, RN extends string, CTX extends Obj> = Record<
+  string,
+  (router: RouterAdapter<Routes, RN, CTX>) => any
+>
 
-type AdapterMap<AD extends Adapters> = {
-  [K in keyof AD]: AD[K] extends Fn<infer A, infer R> ? (...args: A) => R : never
+type AdapterMap<
+  Routes extends RouterShape,
+  RN extends string,
+  CTX extends Obj,
+  AD extends Adapters<Routes, RN, CTX>,
+> = {
+  [K in keyof AD]: AD[K] extends Fn<any, infer R> ? R : never
 }
 
-type API<Routes extends RouterShape, N extends string, CTX extends Obj, AD extends Adapters> = Router<Routes, N, CTX> &
-  AdapterMap<AD>
+export type API<
+  Routes extends RouterShape,
+  N extends string,
+  CTX extends Obj,
+  AD extends Adapters<Routes, N, CTX>,
+> = Router<Routes, N, CTX> & AdapterMap<Routes, N, CTX, AD>
 
 /**
  * Creates a new API instance
@@ -21,7 +33,7 @@ export function create<
   const Routes extends RouterShape,
   const RN extends string,
   const CTX extends Obj,
-  const AD extends Adapters,
+  const AD extends Adapters<Routes, RN, CTX>,
 >(router: RouterAdapter<Routes, RN, CTX>, adapters?: AD): API<Routes, RN, CTX, AD> {
   if (adapters) {
     return omit(
@@ -30,14 +42,14 @@ export function create<
         ...entries(adapters).reduce(
           (acc, [adapterName, adapterVal]) => ({
             ...acc,
-            [adapterName]: adapterVal,
+            [adapterName]: adapterVal(router),
           }),
-          {} as AdapterMap<AD>
+          {} as AdapterMap<Routes, RN, CTX, AD>
         ),
       },
       'find',
       'invoke',
-      'routerMap'
+      'mappedRouter'
     ) as API<Routes, RN, CTX, AD>
   }
   return omit(router, 'find', 'invoke') as API<Routes, RN, CTX, AD>
