759338cab49fabbf3ef80ad7bb2f134985924176
samuelhulla <oficialny.hulla@gmail.com>
2024-04-29 20:08:48 +0200

    :white_check_mark: feat: allow invoking custom calls

--- core/src/custom.ts
+++ core/src/custom.ts
@@ -1,5 +1,5 @@
-import { call, type Resolver } from './call'
-import type { Args, Fn } from './types'
+import { call } from './call'
+import type { Args, Fn, Resolver } from './types'
 
 export function custom<CTX>() {
   return <const N extends string, const M extends string, A extends Args, const R, const R2 = R>(
