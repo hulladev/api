e3450bfae30394027aa208b19fd6c2a6d4fe7ef4
samuelhulla <oficialny.hulla@gmail.com>
2024-04-29 15:51:19 +0200

    :recycle: cleanup: deprecate route api
    move to named procedures/requests

--- core/src/route.ts
+++
@@ -1,9 +0,0 @@
-import { procedure } from './procedure'
-import { request } from './request'
-
-export function route<const CTX>() {
-  return <const N extends string>(route: N) => ({
-    procedure: procedure<CTX, N>(route),
-    request: request<CTX, N>(route),
-  })
-}
