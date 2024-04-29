31d9c422125180c8412dd6102e18a146094d7677
samuelhulla <oficialny.hulla@gmail.com>
2024-04-29 20:10:48 +0200

    :white_check_mark: feat: util functions

--- core/src/util.ts
+++ core/src/util.ts
@@ -1,21 +1,11 @@
-import type { LowercaseMethods } from './constants'
-import type { TypedRequestConfig } from './request'
-import type { Obj } from './types'
+import type { LowercaseMethods, Obj, TypedRequestConfig, URLType } from './types'
 
 export function entries<T extends Obj>(obj: T): [keyof T, T[keyof T]][] {
   return Object.entries(obj) as [keyof T, T[keyof T]][]
 }
 
-export function values<T extends Obj>(obj: T): T[keyof T][] {
-  return Object.values(obj) as T[keyof T][]
-}
-
 export function keys<T extends Obj>(obj: T): (keyof T)[] {
   return Object.keys(obj) as (keyof T)[]
-}
-
-export function assign<T extends Obj, U extends Obj>(obj: T, ...sources: U[]): T & U {
-  return Object.assign(obj, ...sources)
 }
 
 export function omit<T extends Obj, Omitted extends (keyof T)[]>(obj: T, ...omittedKeys: Omitted) {
@@ -32,8 +22,6 @@ export function omit<T extends Obj, Omitted extends (keyof T)[]>(obj: T, ...omit
     {} as Omit<T, Omitted[number]>
   )
 }
-
-export type URLType = InstanceType<typeof URL>
 
 export function isURL(value: URLType | TypedRequestConfig<LowercaseMethods, unknown, unknown>): value is URLType {
   return value instanceof URL
