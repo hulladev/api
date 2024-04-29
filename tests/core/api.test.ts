956d6ac27359a3e1ccc485ec2212033c28a6bde8
samuelhulla <oficialny.hulla@gmail.com>
2024-04-29 16:07:30 +0200

    :test_tube: tests: api sdk tests

--- tests/core/api.test.ts
+++ tests/core/api.test.ts
@@ -8,14 +8,16 @@ describe('main functionality', () => {
     expect(api).toBeDefined()
     expect(api.create).toBeDefined()
     expect(api.context).toBeDefined()
-    expect(api.route).toBeDefined()
+    expect(api.procedure).toBeDefined()
+    expect(api.request).toBeDefined()
     expect(api.router).toBeDefined()
   })
   test('api context is working', () => {
     const ctx = { hello: 'world' }
     const apiWithContext = api.context(ctx)
     expect(apiWithContext.create).toBeDefined()
-    expect(apiWithContext.route).toBeDefined()
+    expect(api.procedure).toBeDefined()
+    expect(api.request).toBeDefined()
     expect(apiWithContext.router).toBeDefined()
     // @ts-expect-error The context method should not be available to prevent .context.context chains
     expect(apiWithContext.context).toBeUndefined()
@@ -23,7 +25,7 @@ describe('main functionality', () => {
   test('api router initializes properly', () => {
     const router = api.router(
       'test',
-      api.route('a').procedure((id: number) => id.toString())
+      api.procedure('a', (id: number) => id.toString())
     )
     expect(router).toBeDefined()
     expect(router.call('a', 2)).toStrictEqual('2')
@@ -32,7 +34,8 @@ describe('main functionality', () => {
     const c = api.context({ hello: 'world' })
     const router = c.router(
       'a',
-      c.route('foo').procedure(
+      c.procedure(
+        'foo',
         () => 'd',
         (_, ctx) => ctx.hello
       )
