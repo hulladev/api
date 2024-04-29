2ede59f501f85bdace7d363a4e029730eb8eccd9
samuelhulla <oficialny.hulla@gmail.com>
2024-04-29 16:15:57 +0200

    :white_check_mark: feat: queryKeys & encodeKeys
    core functionality of key encoding for query and mutations

---
+++ integrations/query/src/keys.ts
@@ -0,0 +1,25 @@
+import type { AvailableCalls, RouteArgs, RouteNamesWithMethod, RouterAdapter, RouterShape } from '@/core/src/router'
+import type { Obj } from '@/core/src/types'
+
+export function encodeKey<const M extends string, const RN extends string, const N extends string>(
+  method: M,
+  router: RN,
+  name: N
+) {
+  return `${method}/${router}/${name}` as const
+}
+
+export function queryKey<const Routes extends RouterShape, const RN extends string, AD extends Obj>(
+  router: RouterAdapter<Routes, RN, AD>,
+  encodeQueryKey = encodeKey
+) {
+  return <
+    const M extends AvailableCalls<Routes>,
+    const N extends RouteNamesWithMethod<Routes, M>,
+    const A extends RouteArgs<Routes, M, N>,
+  >(
+    method: M,
+    name: N,
+    ...args: A
+  ) => [encodeQueryKey(method, router.routerName, name), ...args] as const
+}
