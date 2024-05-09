57f4092b75d17a480ee85faf1328011b292dbf67
samuelhulla <oficialny.hulla@gmail.com>
2024-05-09 22:06:22 +0200

    :wrench: update: variadic length query keys

--- integrations/query/src/keys.ts
+++ integrations/query/src/keys.ts
@@ -1,4 +1,4 @@
-import type { AvailableCalls, Obj, RouteArgs, RouteNamesWithMethod, RouterAdapter, RouterShape } from '@/core/src/types'
+import type { AvailableCalls, Obj, RouteArgs, RouteNamesWithMethod, RouterAdapter, RouterShape } from '@hulla/api'
 import { keys } from './query'
 
 type ReverseTuple<T extends readonly unknown[], R extends any[] = []> = T extends readonly [infer Head, ...infer Tail]
@@ -6,7 +6,8 @@ type ReverseTuple<T extends readonly unknown[], R extends any[] = []> = T extend
   : R
 
 export type QueryKey<T extends readonly unknown[]> =
-  ReverseTuple<T> extends readonly [_, ...infer Rest] ? T | QueryKey<ReverseTuple<Rest>> : []
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  ReverseTuple<T> extends readonly [infer _, ...infer Rest] ? T | QueryKey<ReverseTuple<Rest>> : []
 
 export function encodeKey<const M extends string, const RN extends string, const N extends string>(
   method: M,
@@ -19,7 +20,7 @@ export function encodeKey<const M extends string, const RN extends string, const
 export type KeyMapping<Routes extends RouterShape, RN extends string> = {
   [M in AvailableCalls<Routes>]: <
     const N extends RouteNamesWithMethod<Routes, M>,
-    const RA extends RouteArgs<Routes, M, N>,
+    const RA extends QueryKey<RouteArgs<Routes, M, N>>,
   >(
     route: N,
     ...args: RA
@@ -33,7 +34,7 @@ export function queryKey<const Routes extends RouterShape, const RN extends stri
     (acc, method) => {
       acc[method] = <
         const N extends RouteNamesWithMethod<Routes, typeof method>,
-        A extends RouteArgs<Routes, typeof method, N>,
+        const A extends QueryKey<RouteArgs<Routes, typeof method, N>>,
       >(
         route: N,
         ...args: A
