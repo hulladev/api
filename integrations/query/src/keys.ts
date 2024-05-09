3e2ff03f31f648bf068019a064058e404b6197e3
samuelhulla <oficialny.hulla@gmail.com>
2024-05-09 20:52:35 +0200

    :white_check_mark: feat: new mapping syntax

--- integrations/query/src/keys.ts
+++ integrations/query/src/keys.ts
@@ -1,20 +1,12 @@
 import type { AvailableCalls, Obj, RouteArgs, RouteNamesWithMethod, RouterAdapter, RouterShape } from '@/core/src/types'
+import { keys } from './query'
 
-type TupleVariants<T extends readonly unknown[], U extends readonly unknown[] = []> = T extends readonly [
-  infer Head,
-  ...infer Tail,
-]
-  ? Tail extends readonly unknown[]
-    ? // if u pass the head first, it will be infinitely deep (ts error)
-      // hence we do this and then reverse the tuple order
-      [Head, ...U] | TupleVariants<Tail, [Head, ...U]>
-    : never
-  : []
-type ReverseTuple<T extends any[], R extends any[] = []> = T extends readonly [infer Head, ...infer Tail]
+type ReverseTuple<T extends readonly unknown[], R extends any[] = []> = T extends readonly [infer Head, ...infer Tail]
   ? ReverseTuple<Tail, [Head, ...R]>
   : R
 
-export type QueryKey<T extends readonly unknown[]> = ReverseTuple<TupleVariants<T>, []>
+export type QueryKey<T extends readonly unknown[]> =
+  ReverseTuple<T> extends readonly [_, ...infer Rest] ? T | QueryKey<ReverseTuple<Rest>> : []
 
 export function encodeKey<const M extends string, const RN extends string, const N extends string>(
   method: M,
@@ -24,17 +16,30 @@ export function encodeKey<const M extends string, const RN extends string, const
   return `${method}/${router}/${name}` as const
 }
 
-export function queryKey<const Routes extends RouterShape, const RN extends string, AD extends Obj>(
-  router: RouterAdapter<Routes, RN, AD>,
-  encodeQueryKey = encodeKey
-) {
-  return <
-    const M extends AvailableCalls<Routes>,
+export type KeyMapping<Routes extends RouterShape, RN extends string> = {
+  [M in AvailableCalls<Routes>]: <
     const N extends RouteNamesWithMethod<Routes, M>,
-    const A extends QueryKey<RouteArgs<Routes, M, N>>,
+    const RA extends RouteArgs<Routes, M, N>,
   >(
-    method: M,
-    name: N,
-    ...args: A
-  ) => [encodeQueryKey(method, router.routerName, name), ...args] as const
+    route: N,
+    ...args: RA
+  ) => readonly [`${M}/${RN}/${N}`, ...RA]
+}
+
+export function queryKey<const Routes extends RouterShape, const RN extends string, const AD extends Obj>(
+  router: RouterAdapter<Routes, RN, AD>
+) {
+  return keys(router.mappedRouter).reduce(
+    (acc, method) => {
+      acc[method] = <
+        const N extends RouteNamesWithMethod<Routes, typeof method>,
+        A extends RouteArgs<Routes, typeof method, N>,
+      >(
+        route: N,
+        ...args: A
+      ) => [encodeKey(method, router.routerName, route), ...args] as const
+      return acc
+    },
+    {} as KeyMapping<Routes, RN>
+  )
 }
--- integrations/query/src/mutation.ts
+++ integrations/query/src/mutation.ts
@@ -1,21 +1,10 @@
-import type { AvailableCalls, Obj, RouteArgs, RouteNamesWithMethod, RouterAdapter, RouterShape } from '@hulla/api'
+import type { Obj, RouterAdapter, RouterShape } from '@hulla/api'
 import { encodeKey as defaultEncodeMutationKey } from './keys'
-import { query } from './query'
+import { createMapping } from './query'
 
 export function mutation<Routes extends RouterShape, RN extends string, AD extends Obj>(
   router: RouterAdapter<Routes, RN, AD>,
   encodeMutationKey: typeof defaultEncodeMutationKey = defaultEncodeMutationKey
 ) {
-  return <M extends AvailableCalls<Routes>, N extends RouteNamesWithMethod<Routes, M>>(
-    method: M,
-    name: N,
-    ...args: RouteArgs<Routes, M, N>
-  ) => {
-    // the query function is the same as the mutation function, with just different keys
-    const { queryFn, queryKey } = query(router, encodeMutationKey)(method, name, ...args)
-    return {
-      mutationFn: queryFn,
-      mutationKey: queryKey,
-    }
-  }
+  return createMapping(router, encodeMutationKey, 'mutationKey', 'mutationFn')
 }
