62bc7214e09e5ac15cac0e5d1e71552444101bc6
samuelhulla <oficialny.hulla@gmail.com>
2024-05-09 22:08:00 +0200

    :white_check_mark: feat: swr mutations

--- integrations/swr/src/mutation.ts
+++ integrations/swr/src/mutation.ts
@@ -1,18 +1,10 @@
-import type { AvailableCalls, Obj, RouteArgs, RouteNamesWithMethod, RouterAdapter, RouterShape } from '@hulla/api'
+import type { Obj, RouterAdapter, RouterShape } from '@hulla/api'
 import { encodeKey as defaultEncodeMutationKey } from './keys'
-import { swr } from './swr'
+import { createMapping } from './swr'
 
 export function mutation<Routes extends RouterShape, RN extends string, AD extends Obj>(
   router: RouterAdapter<Routes, RN, AD>,
   encodeMutationKey: typeof defaultEncodeMutationKey = defaultEncodeMutationKey
 ) {
-  return <M extends AvailableCalls<Routes>, N extends RouteNamesWithMethod<Routes, M>>(
-    method: M,
-    name: N,
-    ...args: RouteArgs<Routes, M, N>
-  ) => {
-    // the query function is the same as the mutation function, with just different keys
-    const [mutationKey, mutationFn] = swr(router, encodeMutationKey)(method, name, ...args)
-    return [mutationKey, mutationFn]
-  }
+  return createMapping(router, encodeMutationKey)
 }
