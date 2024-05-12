3981e511d8f4a045aada3ac7ad2192962cdbc919
samuelhulla <oficialny.hulla@gmail.com>
2024-05-12 19:29:10 +0200

    :white_check_mark: feat: method constants

---
+++ packages/request/src/constants.ts
@@ -0,0 +1,13 @@
+import type { LowercaseMethods } from './types'
+
+export const METHODS_LOWERCASE = [
+  'connect',
+  'delete',
+  'get',
+  'head',
+  'options',
+  'patch',
+  'post',
+  'put',
+  'trace',
+] satisfies LowercaseMethods[]
