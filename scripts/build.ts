bde83e8c5d34f2281763fc7642edd3f91d6a9a06
samuelhulla <oficialny.hulla@gmail.com>
2024-05-12 21:52:52 +0200

    :recycle: cleanup: bun release scripts

--- scripts/build.ts
+++
@@ -1,6 +0,0 @@
-import { spawn } from 'bun'
-import { BUILD } from './config' with { type: 'macro' }
-
-const procs = BUILD().map((cmd) => spawn(cmd))
-const builds = await Promise.all(procs.map((proc) => new Response(proc.stdout).text()))
-builds.forEach((pkg) => console.log(pkg))
