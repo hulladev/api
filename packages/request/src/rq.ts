89626dac7288d024a9617fc9db409a2652cbfb2a
samuelhulla <oficialny.hulla@gmail.com>
2024-05-14 17:23:22 +0200

    :white_check_mark: feat: rq util

--- packages/request/src/rq.ts
+++ packages/request/src/rq.ts
@@ -3,8 +3,8 @@ import type { LowercaseMethods, Methods, TypedRequestConfig } from './types'
 export function rq<
   const M extends LowercaseMethods | Methods,
   const U extends string,
-  P extends 'parse' | 'manual' = 'parse',
+  P extends boolean = true,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
->(config: TypedRequestConfig<M, U, P> & { method: M; paramsMode?: P }) {
+>(config: TypedRequestConfig<M, U, P> & { method: M }) {
   return config
 }
