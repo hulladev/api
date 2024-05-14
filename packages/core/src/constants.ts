a2d6f94ec60a784e392676c4ef3d8add59b3ba23
samuelhulla <oficialny.hulla@gmail.com>
2024-05-14 16:59:52 +0200

    :wrench: update: add a satisifies APISDK on reserved keywords
    this guarantees this constant will always be up to date

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,12 +1,9 @@
-import type { Context } from './types'
+import type { APISDK, Context } from './types'
 
-export const RESERVED_CONTEXT = [
-  'route',
-  'method',
-  'route',
-  'args',
-  'type',
-  'routerName',
-] as const satisfies (keyof Context<string, any, any>)[]
+export const RESERVED_CONTEXT = ['route', 'method', 'args', 'type', 'routerName'] as const satisfies (keyof Context<
+  string,
+  any,
+  any
+>)[]
 
-export const RESERVED_KEYWORDS = ['call', 'procedure', 'create'] as const
+export const RESERVED_KEYWORDS = ['call', 'procedure', 'create'] as const satisfies (keyof APISDK<never, never>)[]
