121a9d885b54bbc365f7df2ecf928099a79286a5
samuelhulla <oficialny.hulla@gmail.com>
2024-05-15 12:48:12 +0200

    :wrench: update: adapter properties + types

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -1,9 +1,14 @@
-import type { APISDK, Context } from './types'
+import type { APISDK, AdapterProperties, ContextWithResult } from './types'
 
-export const RESERVED_CONTEXT = ['route', 'method', 'args', 'type', 'routerName'] as const satisfies (keyof Context<
-  string,
-  any,
-  any
->)[]
+export const RESERVED_CONTEXT = [
+  'route',
+  'method',
+  'args',
+  'type',
+  'routerName',
+  'result',
+] as const satisfies (keyof ContextWithResult<any, string, any, any>)[]
 
 export const RESERVED_KEYWORDS = ['call', 'procedure', 'create'] as const satisfies (keyof APISDK<never, never>)[]
+
+export const ADAPTER_PROPERTIES = ['mappedRouter', 'find', 'invoke'] as const satisfies Array<AdapterProperties>
