02766ad86239ab6c1e992fea87c51c1a8c8d3b26
samuelhulla <oficialny.hulla@gmail.com>
2024-05-17 11:22:31 +0200

    :white_check_mark: feat: method override util

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,4 +1,4 @@
-import type { Obj } from './types'
+import type { Args, Call, Context, Obj } from './types'
 
 export function entries<T extends Obj>(obj: T): [keyof T, T[keyof T]][] {
   return Object.entries(obj) as [keyof T, T[keyof T]][]
@@ -21,4 +21,19 @@ export function omit<T extends Obj, Omitted extends (keyof T)[]>(obj: T, ...omit
     },
     {} as Omit<T, Omitted[number]>
   )
+}
+
+export function method<
+  const M extends string,
+  const N extends string,
+  const CM extends string,
+  CTX,
+  const A extends Args,
+  const R,
+  const R2,
+>(method: M, call: Call<N, CM, CTX, A, R, R2>) {
+  type CustomCTX = CTX extends infer Custom & any ? Custom : never
+  // @ts-expect-error overriding defined type method
+  call['method'] = method
+  return call as unknown as Call<N, M, CustomCTX & Context<N, M, A>, A, R, R2>
 }
