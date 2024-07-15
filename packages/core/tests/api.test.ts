d7fa39e21504a048cb92b1ea889902c26621a149
samuelhulla <oficialny.hulla@gmail.com>
2024-07-15 15:31:28 +0200

    :test_tube: tests: new core syntax

--- packages/core/tests/api.test.ts
+++ packages/core/tests/api.test.ts
@@ -1,55 +1,332 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, expectTypeOf, test } from 'vitest'
+import { z } from 'zod'
 import { api } from '../src/api'
 
-// These are general all purpose tests that mainly check user-facing calls behaviour
-// the testing of the individual router methods is done in separate tests
-describe('main functionality', () => {
-  test('api initializes correctly', () => {
+describe('sdk', () => {
+  test('works with 0 args', () => {
     const a = api()
     expect(a).toBeDefined()
-    expect(a.procedure).toBeDefined()
-    expect(a.router).toBeDefined()
+    expect(a.router).toBeTypeOf('function')
+    expect(a.procedure).toBeTypeOf('function')
   })
-  test('api context is working', () => {
-    const context = { hello: 'world' }
-    const apiWithContext = api({ context })
-    expect(apiWithContext.procedure).toBeDefined()
-    expect(apiWithContext.router).toBeDefined()
+  test('works with context', () => {
+    const a = api({ context: { foo: 'foo' } })
+    expect(a).toBeDefined()
+    expect(a.router).toBeTypeOf('function')
+    expect(a.procedure).toBeTypeOf('function')
   })
-  test('api router initializes properly', () => {
-    const router = api().router({ name: 'test', routes: [api().procedure('a', (id: number) => id.toString())] })
-    expect(router).toBeDefined()
-    expect(router.call('a', 2)).toStrictEqual('2')
+})
+
+describe('integrations', () => {
+  const a = api()
+  const router = a.router({
+    name: 'test',
+    routes: [
+      a.procedure('ooo').define(() => null),
+      a
+        .procedure('arg')
+        .input((id: number) => id)
+        .define(({ input }) => input),
+    ],
+    adapters: {
+      query: () => (bar: string) => 'foo' + bar,
+    },
   })
-  test('context is accessible within the router', () => {
-    const c = api({ context: { hello: 'world' } })
-    const router = c.router({
-      name: 'a',
+  test('adapter works', () => {
+    expect(router.query('bar')).toStrictEqual('foobar')
+    expectTypeOf(router.query('bar')).toEqualTypeOf<string>()
+  })
+  test('vanilla calls still work', () => {
+    expect(router.call('ooo')).toBeNull()
+    expectTypeOf(router.call('ooo')).toEqualTypeOf<null>()
+    expect(router.call('arg', 2)).toStrictEqual(2)
+    expectTypeOf(router.call('arg', 2)).toEqualTypeOf<number>()
+  })
+  test('integrations get passed correct context', () => {
+    const b = api({
+      context: { foo: 'foo' },
+    })
+    const r2 = b.router({
+      name: 'withContext',
+      adapters: {
+        query:
+          ({ context }) =>
+          () =>
+            context.foo,
+      },
+      routes: [],
+    })
+    expect(r2.query()).toStrictEqual('foo')
+  })
+  test('adapter can call passed methods', () => {
+    const b = api({
+      context: { foo: 'foo' },
+    })
+    const r2 = b.router({
+      name: 'withContext',
       routes: [
-        c.procedure(
-          'foo',
-          () => 'd',
-          (_, ctx) => ctx.hello
-        ),
+        b
+          .procedure('log')
+          .input(z.string())
+          .define(({ input }) => input),
+      ],
+      adapters: {
+        adp: (adapter) => (bar: string) => adapter.invoke('call', 'log', bar),
+      },
+    })
+    expect(r2.adp('bar')).toStrictEqual('bar')
+    expectTypeOf(r2.adp('bar')).toEqualTypeOf<string>()
+  })
+  test('async context', () => {
+    const b = api({
+      context: {
+        asyncFoo: async () => ({ foo: 'foo' }),
+      },
+    })
+    const r2 = b.router({
+      name: 'asyncContext',
+      routes: [
+        b
+          .procedure('log')
+          .input(z.string())
+          .define(async ({ input, context }) => {
+            const { foo } = await context.asyncFoo()
+            return foo + input
+          }),
       ],
     })
-    expect(router.call('foo')).toStrictEqual('world')
+    expect(r2.call('log', 'bar')).resolves.toStrictEqual('foobar')
   })
-  test('custom methods work', () => {
-    const a = api({
-      methods: () => ({
-        foo: () => 'bar',
-      }),
-    })
-    expect(a.foo()).toStrictEqual('bar')
-  })
-  test('custom methods work with context', () => {
+})
+
+describe('groups', () => {
+  test('groups context ', async () => {
     const a = api({
       context: { foo: 'foo' },
-      methods: (ctx) => ({
-        foo: () => ctx.foo,
-      }),
     })
-    expect(a.foo()).toStrictEqual('foo')
+      .group('obj', { context: { baz: 'baz' } })
+      .group('fn', { context: (options) => ({ ...options.context, bar: 'bar' }) as const })
+      .group('async', { context: async (options) => ({ ...options.context, asyncBar: 'asyncBar' }) as const })
+      .group('noCustomContext')
+    const r = a.router({
+      name: 'test',
+      routes: [
+        a.obj('a').define(({ context }) => context.baz),
+        a.fn('b').define(({ context }) => context.bar),
+        a.async('c').define(({ context }) => context.asyncBar),
+        a.noCustomContext('d').define(({ context }) => context.foo),
+        a.procedure('e').define(({ context }) => context.foo),
+      ],
+    })
+    expect(r.call('a')).toStrictEqual('baz')
+    expectTypeOf(r.call('a')).toEqualTypeOf<'baz'>()
+    expect(r.call('b')).toStrictEqual('bar')
+    expectTypeOf(r.call('b')).toEqualTypeOf<'bar'>()
+    expect(r.call('c')).resolves.toStrictEqual('asyncBar')
+    expectTypeOf(r.call('c')).toEqualTypeOf<Promise<'asyncBar'>>()
+    expect(r.call('d')).toStrictEqual('foo')
+    expectTypeOf(r.call('d')).toEqualTypeOf<'foo'>()
+    expect(r.call('e')).toStrictEqual('foo')
+    expectTypeOf(r.call('e')).toEqualTypeOf<'foo'>()
+  })
+  test('override change show group works', () => {
+    const a = api().group('override', { _override: (options) => () => options.meta.group })
+    expect(a.override()).toStrictEqual('override')
+    expectTypeOf(a.override()).toEqualTypeOf<'override'>()
+    const r = a.router({
+      name: 'test',
+      routes: [a.procedure('a').define(() => null)],
+    })
+    expect(r.call('a')).toBeNull()
+    expectTypeOf(r.call('a')).toEqualTypeOf<null>()
+  })
+  test('default input', () => {
+    const a = api()
+      .group('schema', { defaultInput: z.string() })
+      .group('fn', {
+        defaultInput: (id: string) => {
+          if (typeof id !== 'string') {
+            throw new Error('id must be a string')
+          }
+          return id
+        },
+      })
+    const r = a.router({
+      name: 'test',
+      routes: [
+        a.schema('a').define(({ input }) => input),
+        a.fn('b').define(({ input }) => input),
+        a.procedure('c').define(() => 'noInput' as const),
+      ],
+    })
+    expect(r.call('a', 'bar')).toStrictEqual('bar')
+    expectTypeOf(r.call('a', 'bar')).toEqualTypeOf<string>()
+    expect(r.call('b', 'bar')).toStrictEqual('bar')
+    expectTypeOf(r.call('b', 'bar')).toEqualTypeOf<string>()
+    // @ts-expect-error intentially bad type to throw error
+    expect(() => r.call('a', 2)).toThrow()
+    // @ts-expect-error intentially bad type to throw error
+    expect(() => r.call('b', 2)).toThrow()
+    expect(r.call('c')).toStrictEqual('noInput')
+    expectTypeOf(r.call('c')).toEqualTypeOf<'noInput'>()
+  })
+  test('default input override', () => {
+    const a = api()
+      .group('schema', { defaultInput: z.string() })
+      .group('fn', {
+        defaultInput: (id: string) => {
+          if (typeof id !== 'string') {
+            throw new Error('id must be a string')
+          }
+          return id
+        },
+      })
+    const r = a.router({
+      name: 'test',
+      routes: [
+        a
+          .schema('a')
+          .input(z.number())
+          .define(({ input }) => input),
+        a
+          .fn('b')
+          .input((id: number) => {
+            if (typeof id !== 'number') {
+              throw new Error('id must be a number')
+            }
+            return id
+          })
+          .define(({ input }) => input),
+        a
+          .procedure('c')
+          .input(z.number())
+          .define(({ input }) => input),
+      ],
+    })
+    expect(r.call('a', 2)).toStrictEqual(2)
+    expectTypeOf(r.call('a', 2)).toEqualTypeOf<number>()
+    expect(r.call('b', 2)).toStrictEqual(2)
+    expectTypeOf(r.call('b', 2)).toEqualTypeOf<number>()
+    expect(r.call('c', 2)).toStrictEqual(2)
+    expectTypeOf(r.call('c', 2)).toEqualTypeOf<number>()
+    // @ts-expect-error intentially bad type to throw error
+    expect(() => r.call('a', 'bar')).toThrow()
+    // @ts-expect-error intentially bad type to throw error
+    expect(() => r.call('b', 'bar')).toThrow()
+    // @ts-expect-error intentially bad type to throw error
+    expect(() => r.call('c', 'bar')).toThrow()
+  })
+  test('default output', () => {
+    const a = api()
+      .group('schema', { defaultOutput: z.string() })
+      .group('fn', {
+        defaultOutput: (id: string) => {
+          if (typeof id !== 'string') {
+            throw new Error('id must be a string')
+          }
+          return id
+        },
+      })
+    const r = a.router({
+      name: 'test',
+      routes: [a.schema('a').define(() => 'a'), a.fn('b').define(() => 'bar'), a.procedure('c').define(() => 'bar')],
+    })
+    expect(r.call('a')).toStrictEqual('a')
+    expectTypeOf(r.call('a')).toEqualTypeOf<string>()
+    expect(r.call('b')).toStrictEqual('bar')
+    expectTypeOf(r.call('b')).toEqualTypeOf<string>()
+    expect(r.call('c')).toStrictEqual('bar')
+    expectTypeOf(r.call('c')).toEqualTypeOf<string>()
+    expect(() =>
+      a
+        .schema('a')
+        // @ts-expect-error intentially bad type to throw error
+        .define(() => 2)
+        .fn()
+    ).toThrow()
+  })
+  test('default output override', () => {
+    const a = api()
+      .group('schema', { defaultOutput: z.string() })
+      .group('fn', {
+        defaultOutput: (id: string) => {
+          if (typeof id !== 'string') {
+            throw new Error('id must be a string')
+          }
+          return id
+        },
+      })
+    const r = a.router({
+      name: 'test',
+      routes: [
+        a
+          .schema('a')
+          .output(z.number())
+          .define(() => 2),
+        a
+          .fn('b')
+          .output(z.number())
+          .define(() => 2),
+        a
+          .procedure('c')
+          .output(z.number())
+          .define(() => 2),
+      ],
+    })
+    expect(r.call('a')).toStrictEqual(2)
+    expectTypeOf(r.call('a')).toEqualTypeOf<number>()
+    expect(r.call('b')).toStrictEqual(2)
+    expectTypeOf(r.call('b')).toEqualTypeOf<number>()
+    expect(r.call('c')).toStrictEqual(2)
+    expectTypeOf(r.call('c')).toEqualTypeOf<number>()
+    expect(() =>
+      a
+        .schema('a')
+        .output(z.number())
+        // @ts-expect-error intentially bad type to throw error
+        .define(() => 'sss')
+        .fn()
+    ).toThrow()
+  })
+})
+
+describe('default method', () => {
+  test('global methods', () => {
+    const a = api({
+      defaultMethod: 'boo',
+    })
+    const r = a.router({
+      name: 'test',
+      routes: [a.procedure('a').define(({ meta }) => meta.method)],
+    })
+    expect(r.boo).toBeTypeOf('function')
+    expect(r.boo('a')).toStrictEqual('boo')
+    expectTypeOf(r.boo('a')).toEqualTypeOf<'boo'>()
+  })
+  test('group default method', () => {
+    const a = api().group('boo', { defaultMethod: 'boo' }).group('foo', { defaultMethod: 'foo' })
+    const r = a.router({
+      name: 'test',
+      routes: [a.boo('a').define(({ meta }) => meta.method), a.foo('b').define(({ meta }) => meta.method)],
+    })
+    expect(r.boo).toBeTypeOf('function')
+    expect(r.foo).toBeTypeOf('function')
+    expect(r.boo('a')).toStrictEqual('boo')
+    expectTypeOf(r.boo('a')).toEqualTypeOf<'boo'>()
+    expect(r.foo('b')).toStrictEqual('foo')
+    expectTypeOf(r.foo('b')).toEqualTypeOf<'foo'>()
+  })
+  test('group with global default method', () => {
+    const a = api({ defaultMethod: 'boo' }).group('foo', { defaultMethod: 'foo' })
+    const r = a.router({
+      name: 'test',
+      routes: [a.procedure('a').define(({ meta }) => meta.method), a.foo('b').define(({ meta }) => meta.method)],
+    })
+    expect(r.boo).toBeTypeOf('function')
+    expect(r.foo).toBeTypeOf('function')
+    expect(r.boo('a')).toStrictEqual('boo')
+    expectTypeOf(r.boo('a')).toEqualTypeOf<'boo'>()
+    expect(r.foo('b')).toStrictEqual('foo')
+    expectTypeOf(r.foo('b')).toEqualTypeOf<'foo'>()
   })
 })
--- packages/core/tests/procedure.test.ts
+++ packages/core/tests/procedure.test.ts
@@ -1,64 +1,270 @@
-import { expectTypeOf } from 'expect-type'
-import { describe, expect, test } from 'vitest'
+// @ts-check
+import { describe, expect, expectTypeOf, test } from 'vitest'
+import { z } from 'zod'
 import { procedure } from '../src/procedure'
-import type { Context } from '../src/types'
 
-const ctx: Context<'foo', 'call', [id: number], 'router'> = {
-  method: 'call',
-  type: 'procedure',
-  route: 'foo',
-  routerName: 'router',
-  args: [1],
-}
+const context = { foo: 'foo' } as const
+const parseKey = 'parse'
+const p = procedure({ context, parseKey, group: 'procedure', defaultMethod: 'call', defaultContext: context })
 
-const testProcedure = procedure()('foo', (id: number) => !!id)
-const withResolver = procedure<typeof ctx>()(
-  'foo',
-  (id: number) => id,
-  // note the ctx will be passed from router in practice, but here in unit test we mock it
-  (id) => ({ id, ctx })
-)
-
-// there's a big overlap with the call() tests, so we're keeping this brief
-
-describe('functionality', () => {
-  test('procedure has correct function return', () => {
-    expect(testProcedure.fn(1)).toStrictEqual(true)
-    expect(testProcedure.fn(0)).toStrictEqual(false)
-    expect(withResolver.fn(1)).toStrictEqual(1)
+/**
+ * these tests focus on the user-DX of having correct chains available
+ */
+describe('procedure chaining', () => {
+  const px = p('hello')
+  test('top level', () => {
+    expect(px.define).toBeTypeOf('function')
+    expect(px.input).toBeTypeOf('function')
+    expect(px.output).toBeTypeOf('function')
   })
-  test('procedure has correct internal call name', () => {
-    expect(testProcedure.method).toStrictEqual('call')
-    expect(withResolver.method).toStrictEqual('call')
+  test('direct define', () => {
+    const direct = px.define(() => 'foo')
+    // @ts-expect-error intentional bad call
+    expect(direct.define).toBeUndefined()
+    // @ts-expect-error intentional bad call
+    expect(direct.input).toBeUndefined()
+    // @ts-expect-error intentional bad call
+    expect(direct.output).toBeUndefined()
+  })
+  test('input chains', () => {
+    // top level input chains:
+    const pi = px.input((id: string) => id)
+    // @ts-expect-error intentional bad call
+    expect(pi.input).toBeUndefined()
+    expect(pi.define).toBeTypeOf('function')
+    expect(pi.output).toBeTypeOf('function')
+    // now input -> output chain
+    const pio = pi.output(() => 2)
+    expect(pio.define).toBeTypeOf('function')
+    // @ts-expect-error intentional bad call
+    expect(pio.input).toBeUndefined()
+    // @ts-expect-error intentional bad call
+    expect(pio.output).toBeUndefined()
+  })
+  test('output chains', () => {
+    //top level output chains
+    const po = px.output((x: number) => x)
+    expect(po.define).toBeTypeOf('function')
+    expect(po.input).toBeTypeOf('function')
+    // @ts-expect-error intentional bad call
+    expect(po.output).toBeUndefined()
+    // now output -> input chain
+    const poi = po.input((id: string) => id)
+    expect(poi.define).toBeTypeOf('function')
+    // @ts-expect-error intentional bad call
+    expect(poi.input).toBeUndefined()
+    // @ts-expect-error intentional bad call
+    expect(poi.output).toBeUndefined()
   })
 })
 
-// note these will always pass in a test suite - however they do throw a type error in IDE/build time
-// which we can detect in ci/cd
-describe('types', () => {
-  test('procedure has correct function type', () => {
-    expectTypeOf(testProcedure.fn).toEqualTypeOf<(id: number) => boolean>()
-    expectTypeOf(withResolver.fn).toEqualTypeOf<(id: number) => number>()
+/**
+ * these tests focus on the baisc calls having correct types and values
+ */
+describe('types and values', () => {
+  const px = p('test')
+  test('top level', () => {
+    const pd = px.define(() => null)
+    expect(pd.name).toStrictEqual('test')
+    expectTypeOf(pd.name).toEqualTypeOf<'test'>()
+    expect(pd.method).toStrictEqual('call')
+    expectTypeOf(pd.method).toEqualTypeOf<'call'>()
+    expect(pd.fn()).toBeNull()
+    expectTypeOf(pd.fn()).toEqualTypeOf<null>()
   })
-  test('procedure has correct return', () => {
-    expectTypeOf(testProcedure.fn).returns.toEqualTypeOf<boolean>()
-    expectTypeOf(withResolver.fn).returns.toEqualTypeOf<number>()
+  test('input', () => {
+    // top level input chain
+    const pi = px.input((num: number) => num)
+    const pd = pi.define(({ input }) => input.toString())
+    expect(pd.name).toStrictEqual('test')
+    expectTypeOf(pd.name).toEqualTypeOf<'test'>()
+    expect(pd.method).toStrictEqual('call')
+    expectTypeOf(pd.method).toEqualTypeOf<'call'>()
+    expect(pd.fn(2)).toStrictEqual('2')
+    expectTypeOf(pd.fn(2)).toEqualTypeOf<string>()
+    // nested input chain
+    const pio = pi.output((bool: boolean) => !!bool)
+    const pd2 = pio.define(({ input }) => !!input)
+    expect(pd2.name).toStrictEqual('test')
+    expectTypeOf(pd2.name).toEqualTypeOf<'test'>()
+    expect(pd2.method).toStrictEqual('call')
+    expectTypeOf(pd2.method).toEqualTypeOf<'call'>()
+    expect(pd2.fn(2)).toStrictEqual(true)
+    expectTypeOf(pd2.fn(2)).toEqualTypeOf<boolean>()
   })
-  test('procedure has correct internal call name type', () => {
-    expectTypeOf(testProcedure.method).toEqualTypeOf<'call'>()
+  test('output', () => {
+    // top level output chain
+    const po = px.output((num: number) => num)
+    const pd = po.define(() => 2)
+    expect(pd.name).toStrictEqual('test')
+    expectTypeOf(pd.name).toEqualTypeOf<'test'>()
+    expect(pd.method).toStrictEqual('call')
+    expectTypeOf(pd.method).toEqualTypeOf<'call'>()
+    expect(pd.fn()).toStrictEqual(2)
+    expectTypeOf(pd.fn()).toEqualTypeOf<number>()
+    // nested output chain
+    const poi = po.input((bool: boolean) => !!bool)
+    const pd2 = poi.define(({ input }) => +input)
+    expect(pd2.name).toStrictEqual('test')
+    expectTypeOf(pd2.name).toEqualTypeOf<'test'>()
+    expect(pd2.method).toStrictEqual('call')
+    expectTypeOf(pd2.method).toEqualTypeOf<'call'>()
+    expect(pd2.fn(true)).toStrictEqual(1)
+    expectTypeOf(pd2.fn(true)).toEqualTypeOf<number>()
   })
-  test('the context has correct type', () => {
-    expectTypeOf(withResolver.resolver)
-      .parameter(1)
-      .toEqualTypeOf<Context<'foo', 'call', [id: number], 'router'> & Context<'foo', 'call', readonly [id: number]>>()
+  test('schemas', () => {
+    const pi = px.input(z.number())
+    const pd = pi.define(({ input }) => input.toString())
+    expect(pd.name).toStrictEqual('test')
+    expectTypeOf(pd.name).toEqualTypeOf<'test'>()
+    expect(pd.method).toStrictEqual('call')
+    expectTypeOf(pd.method).toEqualTypeOf<'call'>()
+    expect(pd.fn(2)).toStrictEqual('2')
+    expectTypeOf(pd.fn(2)).toEqualTypeOf<string>()
+    const po = px.output(z.number())
+    const pd2 = po.define(() => 2)
+    expect(pd2.name).toStrictEqual('test')
+    expectTypeOf(pd2.name).toEqualTypeOf<'test'>()
+    expect(pd2.method).toStrictEqual('call')
+    expectTypeOf(pd2.method).toEqualTypeOf<'call'>()
+    expect(pd2.fn()).toStrictEqual(2)
+    expectTypeOf(pd2.fn()).toEqualTypeOf<number>()
+    const pio = pi.output(z.boolean())
+    const pd3 = pio.define(({ input }) => !!input)
+    expect(pd3.name).toStrictEqual('test')
+    expectTypeOf(pd3.name).toEqualTypeOf<'test'>()
+    expect(pd3.method).toStrictEqual('call')
+    expectTypeOf(pd3.method).toEqualTypeOf<'call'>()
+    expect(pd3.fn(2)).toStrictEqual(true)
+    expectTypeOf(pd3.fn(2)).toEqualTypeOf<boolean>()
+    const poi = po.input(z.boolean())
+    const pd4 = poi.define(({ input }) => +input)
+    expect(pd4.name).toStrictEqual('test')
+    expectTypeOf(pd4.name).toEqualTypeOf<'test'>()
+    expect(pd4.method).toStrictEqual('call')
+    expectTypeOf(pd4.method).toEqualTypeOf<'call'>()
+    expect(pd4.fn(true)).toStrictEqual(1)
+    expectTypeOf(pd4.fn(true)).toEqualTypeOf<number>()
   })
 })
 
-describe('advanced functionality', () => {
-  test('resolver works correctly', () => {
-    expect(
-      // we mocked the passed context here - this will be passed from the router in practice
-      withResolver.resolver?.(withResolver.fn(1), ctx)
-    ).toStrictEqual({ id: 1, ctx })
+/**
+ * tests targetting method definitions
+ */
+describe('method overrides', () => {
+  test('method override', () => {
+    const pd = p('test', 'get').define(() => null)
+    expect(pd.method).toStrictEqual('get')
+    expectTypeOf(pd.method).toEqualTypeOf<'get'>()
+    const pi = p('test', 'get').input((num: number) => num)
+    const pd2 = pi.define(({ input }) => input.toString())
+    expect(pd2.method).toStrictEqual('get')
+    expectTypeOf(pd2.method).toEqualTypeOf<'get'>()
+    const po = p('test', 'get').output((num: number) => num)
+    const pd3 = po.define(() => 2)
+    expect(pd3.method).toStrictEqual('get')
+    expectTypeOf(pd3.method).toEqualTypeOf<'get'>()
+    const pio = pi.output(z.boolean())
+    const pd4 = pio.define(({ input }) => !!input)
+    expect(pd4.method).toStrictEqual('get')
+    expectTypeOf(pd4.method).toEqualTypeOf<'get'>()
+    const poi = po.input(z.boolean())
+    const pd5 = poi.define(({ input }) => +input)
+    expect(pd5.method).toStrictEqual('get')
+    expectTypeOf(pd5.method).toEqualTypeOf<'get'>()
+  })
+})
+
+/**
+ * input, output, function, schema -> all kinds of validation
+ */
+describe('validation', () => {
+  test('function validator input', () => {
+    const pi = p('test').input((num: number) => {
+      if (num > 2) {
+        throw new Error('too big')
+      }
+      return num
+    })
+    const pd = pi.define(({ input }) => input)
+    expect(pd.fn(1)).toStrictEqual(1)
+    expectTypeOf(pd.fn(1)).toEqualTypeOf<number>()
+    expect(() => pd.fn(3)).toThrow('too big')
+    const pio = pi.output((bool: boolean) => {
+      if (!bool) throw new Error('not true')
+      return bool as boolean
+    })
+    const pd2 = pio.define(({ input }) => !!input)
+    expect(pd2.fn(1)).toStrictEqual(true)
+    expectTypeOf(pd2.fn(1)).toEqualTypeOf<boolean>()
+    expect(() => pd2.fn(3)).toThrow('too big')
+  })
+  test('function validator output', () => {
+    const po = p('test').output((num: number) => {
+      if (num > 2) {
+        throw new Error('too big')
+      }
+      return num
+    })
+    const pd = po.define(() => 1)
+    expect(pd.fn()).toStrictEqual(1)
+    expectTypeOf(pd.fn()).toEqualTypeOf<number>()
+    const pdBad = po.define(() => 3)
+    expect(() => pdBad.fn()).toThrow('too big')
+    // with input attached (nested chain)
+    const pi = po.input((num: string) => {
+      if (isNaN(+num)) {
+        throw new Error('not a number')
+      }
+      return num
+    })
+    const pd2 = pi.define(({ input }) => +input)
+    expect(pd2.fn('1')).toStrictEqual(1)
+    expectTypeOf(pd2.fn('1')).toEqualTypeOf<number>()
+    expect(() => pd2.fn('3')).toThrow('too big')
+  })
+  test('schema validator input', () => {
+    const pi = p('test').input(z.number())
+    const pd = pi.define(({ input }) => input)
+    expect(pd.fn(1)).toStrictEqual(1)
+    expectTypeOf(pd.fn(1)).toEqualTypeOf<number>()
+    // @ts-expect-error intentional bad call
+    expect(() => pd.fn('3')).toThrow()
+    const pio = pi.output(z.boolean())
+    const pd2 = pio.define(({ input }) => !!input)
+    expect(pd2.fn(1)).toStrictEqual(true)
+    expectTypeOf(pd2.fn(1)).toEqualTypeOf<boolean>()
+    // @ts-expect-error intentional bad call
+    expect(() => pd2.fn('3')).toThrow()
+  })
+  test('schema validator output', () => {
+    const po = p('test').output(z.number().max(2))
+    const pd = po.define(() => 1)
+    expect(pd.fn()).toStrictEqual(1)
+    expectTypeOf(pd.fn()).toEqualTypeOf<number>()
+    const pdBad = po.define(() => 3)
+    expect(() => pdBad.fn()).toThrow()
+    // with input attached (nested chain)
+    const pi = po.input(z.string())
+    const pd2 = pi.define(({ input }) => +input)
+    expect(pd2.fn('1')).toStrictEqual(1)
+    expectTypeOf(pd2.fn('1')).toEqualTypeOf<number>()
+    expect(() => pd2.fn('3')).toThrow()
+  })
+  test('change parseKey access', () => {
+    const pk = procedure({
+      context,
+      parseKey: '_parse',
+      group: 'procedure',
+      defaultMethod: 'call',
+      defaultContext: context,
+    })
+    const schema = z.number().max(2)
+    const changedSchema = { _parse: schema.parse }
+    const pi = pk('test').input(changedSchema)
+    const pd = pi.define(({ input }) => input)
+    expect(pd.fn(1)).toStrictEqual(1)
+    expectTypeOf(pd.fn(1)).toEqualTypeOf<number>()
+    expect(() => pd.fn(3)).toThrow()
   })
 })
