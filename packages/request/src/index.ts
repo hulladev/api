acb862b39466aa5122072f55135e5f79be999c03
samuelhulla <oficialny.hulla@gmail.com>
2024-07-24 15:53:15 +0200

    :recycle: cleanup: archive the legacy request package

--- packages/request/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { request } from './request'
-export { resolve } from './resolve'
-export { addParams, parseBody, parseRequest, parseUrl, response } from './response'
-export { rq } from './rq'
-export type * from './types'
